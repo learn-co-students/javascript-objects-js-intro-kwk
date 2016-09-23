037b7bc9ab2a050adcf8728b81dbad818d24f064
S D <santid13@hotmail.com>
2016-09-23 03:40:26 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,17 @@
+
+var playlist = {bovi: 'my life', perry: 'fireworks', kim: 'dance' };
+
+
+
+function updatePlaylist(playlist, artistName, songTitle) {
+playlist['Phil Ochs'] = 'Here\'s to the State of Mississippi';
+
+return playlist
+}
+
+
+function removeFromPlaylist(playlist, Kanye)
+{
+ delete playlist.Kanye;
+ return playlist;
+}
