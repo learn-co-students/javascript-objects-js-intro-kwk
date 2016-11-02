f6738b8c8f7cc6fb3ed7f00b9a66b6a9a66c0730
Jenna Hegarty <jmghga@gmail.com>
2016-11-02 15:03:36 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = { 'Elton John' : 'Yellow Brick Road'};
+
+function updatePlaylist(playlist, artistName, songTitle) {}
+return Object.assign({}, playlist, { [artistName]: songTitle });
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist.artistName;
+}
