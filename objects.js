e1ddcac418080f8aa5e00591ff95f0128d8094b6
ivalentine <ian@ivalentine.net>
2016-12-20 17:32:54 -0500

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {
+  "The Long Winters": "Pushover",
+}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+}
