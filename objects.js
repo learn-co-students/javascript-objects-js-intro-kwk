b12ff1d5fd91ab63443a397f050fc3a10a1d62b3
Kimia Kalantari <kimiakalantari1356@gmail.com>
2016-12-27 00:20:04 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,2 +1,6 @@
 var playlist = {Jackson: "Earth Song"}; 
 
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistNamename] = songTitle
+  return updatePlaylist(playlist, "Jackson2", "Remember the Time")
+}
