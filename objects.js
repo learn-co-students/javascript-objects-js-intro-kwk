3b2a2dd3f283cdfff5cb2cd7b436f84306174ad2
Kimia Kalantari <kimiakalantari1356@gmail.com>
2016-12-27 15:53:11 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,11 +1,11 @@
-var playlist = {Phil Ochs: "Slowdive"}; 
+var playlist = {["Phil Ochs"]: "My Bloody Valentine"};
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist.PhilOchs = "My Bloody Valentine"; 
-  return playlist  
+  playlist["Phil Ochs"] = "My Bloody Valentine";
+  return playlist
 }
 
 function removeFromPlaylist(playlist, artisName){
-  delete playlist.Jackson; 
+  delete playlist["Slowdive"];
   return playlist
-} 
+}
