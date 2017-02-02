f0256017f4de47f0b1009383630467e48fc8fd25
Sophia H <shhubeny@gmail.com>
2017-02-02 18:08:42 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,2 +1,10 @@
-var playlist = {Mozart:"Violin Concerto"};
+var playlist = {"Mozart":"Violin Concerto"};
 
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName]=songTitle;
+  return playlist
+}
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
